0e625e3ff4d680dca52e2f1e2a2016050171483f
Ales Horna <ah.horna@gmail.com>
2026-05-04 21:30:42 +0200

    feat: add orcid endpoint

---
+++ api/src/users-module/controllers/user-orcid.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { UserDto } from '../dtos/user.dto';
+import { RequestUser } from '../../auth-module/decorators/user.decorator';
+import { MinRoleCheck } from '../../permission-module/decorators/min-role.decorator';
+import { RoleEnum } from '../../permission-module/models/role.enum';
+
+@Controller('/users')
+@ApiTags('Users')
+export class UserOrcidController {
+	constructor() {}
+
+	@Get('orcid')
+	@MinRoleCheck(RoleEnum.USER)
+	@ApiOperation({
+		summary: 'Get current user\'s ORCID.',
+		description: 'Returns the ORCID of the authenticated user.'
+	})
+	@ApiOkResponse({
+		description: 'User ORCID.',
+		schema: {
+			type: 'object',
+			properties: {
+				orcid: {
+					type: 'string',
+					example: '0000-0002-3237-9305'
+				}
+			}
+		}
+	})
+	public async getUserOrcid(
+		@RequestUser() user: UserDto) {
+		return { orcid: user.orcid || null };
+	}
+}
